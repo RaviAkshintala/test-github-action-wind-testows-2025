119e2dad3be1d5499d3e4ced42bec960ed8ddff4
RaviAkshintala <167848931+RaviAkshintala@users.noreply.github.com>
2025-03-18 10:31:05 +0530

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,40 +1,12 @@
-using System;
 using CommandLine;
+using ConsoleApp;
 
-class Program
+var arguments = Parser.Default.ParseArguments<ProgramArgumentsReadDto>(args)?.Value;
+if (arguments == null)
 {
-    public class Options
-    {
-        [Option('f', "flavor", Required = true, HelpText = "Which flavor of the artifact should be created by the setup.")]
-        public string Flavor { get; set; }
-
-        [Option('e', "environment", Required = true, HelpText = "Whether the final installed artifact should point to staging or production apis, and whether the final artifact should be uploaded to staging or production backoffice.")]
-        public string Environment { get; set; }
-
-        [Option('v', "version", Required = true, HelpText = "The sem-ver of the artifact. Will also be appended to the setup filename.")]
-        public string Version { get; set; }
-    }
-
-    static void Main(string[] args)
-    {
-        Parser.Default.ParseArguments<Options>(args)
-            .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts))
-            .WithNotParsed<Options>((errs) => HandleParseError(errs));
-    }
-
-    static void RunOptionsAndReturnExitCode(Options opts)
-    {
-        Console.WriteLine($"flavor: {opts.Flavor}");
-        Console.WriteLine($"environment: {opts.Environment}");
-        Console.WriteLine($"version: {opts.Version}");
-    }
-
-    static void HandleParseError(IEnumerable<Error> errs)
-    {
-        foreach (var err in errs)
-        {
-            Console.WriteLine(err.ToString());
-        }
-        throw new Exception("[DEV] could not parse arguments");
-    }
+    throw new Exception("[DEV] could not parse arguments");
 }
+
+Console.WriteLine($"flavor: {arguments.Flavor}");
+Console.WriteLine($"environment: {arguments.Environment}");
+Console.WriteLine($"version: {arguments.Version}");
